83a5f0c7e06f1eff32259764f279ec3a7ef6ef71
Thomas Nowak <thomas@thomasnowak.net>
2024-12-06 11:15:02 +0100

    Fixed typo in import statement

--- solve_ivp_pint/__init__.py
+++ solve_ivp_pint/__init__.py
@@ -1,6 +1,6 @@
 """solve_ivp_pint : ODE solver using units with Pint"""
 
-from solve_ivp.solve_ivp_pint import solve_ivp
+from solve_ivp_pint.solve_ivp_pint import solve_ivp
 
 __all__ = [
     "solve_ivp",
