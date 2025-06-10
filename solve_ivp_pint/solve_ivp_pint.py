decfd7672d9afa7de84d4255235a92d590e6bbfd
Matthias Fuegger <mfuegger@lmf.cnrs.fr>
2025-06-10 17:16:34 +0200

    more typing

--- solve_ivp_pint/solve_ivp_pint.py
+++ solve_ivp_pint/solve_ivp_pint.py
@@ -2,14 +2,17 @@
 
 from collections.abc import Callable, Sequence
 
+import numpy as np
 import pint
 import scipy.integrate
 from pint import Quantity, UnitRegistry
+from scipy.optimize import OptimizeResult
 
 
 def factory(
-    model, t_span0: Quantity | list[Quantity] | tuple[Quantity], x_0: Sequence[Quantity], ureg: UnitRegistry
+    model: Callable, t_span0: Quantity | list[Quantity] | tuple[Quantity], x_0: Sequence[Quantity], ureg: UnitRegistry
 ) -> tuple:
+    """Create the unitless integration objects."""
     # Delete t_span0 and x_0 units (if any)
     x0_no_units = [item.magnitude for item in x_0]
     x0_units = [item.units for item in x_0]
@@ -34,34 +37,34 @@ def factory(
         raise TypeError(msg)
 
     # Defines f_no_units as a closure
-    def f_no_units(t, x, *args) -> list:
+    def f_no_units(t: np.number, x: np.ndarray | tuple[float], *args: tuple) -> list:
         # Use the captured x_0 and t_span0
-        x_units = [val * ureg.Unit(str(ref.units)) for val, ref in zip(x, x_0)]
+        x_units = [val * ureg.Unit(str(ref.units)) for val, ref in zip(x, x_0, strict=False)]
 
         # Calculate derivatives
         dxdt_with_units = model(t, x_units, *args)
 
         return [
             term.to(ref.units / t_span_units).magnitude if not term.dimensionless else term.magnitude
-            for term, ref in zip(dxdt_with_units, x_0)
+            for term, ref in zip(dxdt_with_units, x_0, strict=False)
         ]
 
     return f_no_units, x0_no_units, t_span_no_units, t_span_units, x0_units
 
 
-def solve_ivp(
+def solve_ivp(  # noqa: PLR0913
     fun: Callable,
     t_span: list[Quantity] | tuple[Quantity],
-    y0,
+    y0: list[Quantity] | tuple[Quantity],
     *,
     method: str="RK45",
-    t_eval=None,
-    dense_output=False,
-    events=None,
-    vectorized=False,
-    args=None,
-    **options,
-):
+    t_eval: Quantity | None = None,
+    dense_output: bool=False,
+    events: Callable | list[Callable] | None=None,
+    vectorized: bool=False,
+    args: tuple | None =None,
+    **options,  # noqa: ANN003
+) -> OptimizeResult:
     """A solve_ivp function with pint units."""
     # Check of t_span's type
     if not isinstance(t_span, (list, tuple)):
@@ -73,7 +76,7 @@ def solve_ivp(
         msg = f"Expected t_span to contain exactly two elements, but got {len(t_span)}"
         raise ValueError(msg)
 
-    # Check that each t_span's element has an attribut '_REGISTRY'
+    # Check that each t_span's element has an attribute '_REGISTRY'
     for i, t in enumerate(t_span):
         if not hasattr(t, "_REGISTRY"):
             msg = f"The element t_span[{i}] ({t}) does not have a '_REGISTRY' attribute. Ensure it has units."
@@ -96,13 +99,13 @@ def solve_ivp(
             # Check the compatibility between t_eval & t_span
             if not t_eval.check(t_span_units):
                 # Conversion of t_eval to have the same units as t_span
-                t_eval = t_eval.to(t_span_units)
+                t_eval = t_eval.to(t_span_units) # type: ignore
         except pint.errors.DimensionalityError as e:
             # Will give an explicit pint error if the conversion fails
             msg = f"Failed to convert units of t_eval to match t_span. Error: {e}, please check the unit of t_eval, it should be the same as t_span"
             raise ValueError(msg) from e
 
-        t_eval = t_eval.magnitude  # Convert to values without units
+        t_eval = t_eval.magnitude  # type: ignore # Convert to values without units
 
     # Calling 'solve_ivp' to solve ODEs
     solution_sys = scipy.integrate.solve_ivp(
@@ -125,6 +128,6 @@ def solve_ivp(
 
     # Add units back in to solution
     solution_sys.t = [time * t_span_units for time in solution_sys.t]
-    solution_sys.y = [[val * unit for val in vals] for vals, unit in zip(solution_sys.y, x0_units)]
+    solution_sys.y = [[val * unit for val in vals] for vals, unit in zip(solution_sys.y, x0_units, strict=False)]
 
     return solution_sys
